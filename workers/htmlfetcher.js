1526a35c7bda2cb5efa593ca7356b874ebf4df72
MKS-Student <mks.student@hackreactor.com>
2015-03-28 12:17:51 -0500

    fixed cron bug

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,6 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+var archive = require('/Users/student/Documents/MKS14-web-historian/helpers/archive-helpers');
+
+archive.downloadUrls();
